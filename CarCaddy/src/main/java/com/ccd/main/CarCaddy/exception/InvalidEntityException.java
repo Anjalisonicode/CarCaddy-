f2c0f97f2567d6e685afcae69d8f8f01c1f8628d
Anjalisonicode <151420617+Anjalisonicode@users.noreply.github.com>
2025-01-06 14:08:44 +0530

    Delete CarCaddy directory

--- CarCaddy/src/main/java/com/ccd/main/CarCaddy/exception/InvalidEntityException.java
+++
@@ -1,7 +0,0 @@
-package com.ccd.main.CarCaddy.exception;
-
-public class InvalidEntityException extends RuntimeException { // Extend RuntimeException for unchecked exception
-    public InvalidEntityException(String msg) {
-        super(msg); // Pass the error message to the superclass
-    }
-}
--- CarCaddy/src/main/java/com/ccd/main/CarCaddy/model/Car.java
+++
@@ -1,4 +0,0 @@
-package com.ccd.main.CarCaddy.model;
-
-public class Car {
-}
