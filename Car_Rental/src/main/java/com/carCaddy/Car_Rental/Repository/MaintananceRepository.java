3e12c000d0be1860b2595e09247467bde2880b5c
Anjalisonicode <151420617+Anjalisonicode@users.noreply.github.com>
2025-01-19 09:46:03 +0530

    Delete Car_Rental directory

--- Car_Rental/src/main/java/com/carCaddy/Car_Rental/Repository/MaintananceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carCaddy.Car_Rental.Repository;
-
-import com.carCaddy.Car_Rental.Model.Maintainance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaintananceRepository extends JpaRepository<Maintainance, Integer> {
-}
